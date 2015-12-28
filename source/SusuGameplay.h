4820107b4ab81d73239f3e5365af072f4fe0529a
adrianlwn <adrian.lwn@gmail.com>
2015-12-28 22:06:27 +0000

    Bounce#3

---
+++ source/SusuGameplay.h
@@ -0,0 +1,13 @@
+/*
+ * SusuGameplay.h
+ *
+ *  Created on: 11 déc. 2015
+ *      Author: valentine
+ */
+
+#ifndef SUSUWATARI_SUSUGAMEPLAY_H_
+#define SUSUWATARI_SUSUGAMEPLAY_H_
+
+
+
+#endif /* SUSUWATARI_SUSUGAMEPLAY_H_ */
