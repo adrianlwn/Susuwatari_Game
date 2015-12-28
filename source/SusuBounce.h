753b9cafbbd5ee39e40b1af2c560b546ef3b035e
adrianlwn <adrian.lwn@gmail.com>
2015-12-28 21:27:43 +0000

    Revert "Temporaire #2"
    This reverts commit d6d62cec600b87b948861f24a69b0b9f35df1fdf.

--- source/SusuBounce.h
+++ source/SusuBounce.h
@@ -1,5 +1,5 @@
 /*
- * SusuGameplay.h
+ * SusuBounce.h
  *
  *  Created on: 11 déc. 2015
  *      Author: valentine
@@ -8,6 +8,11 @@
 #ifndef SUSUWATARI_SUSUGAMEPLAY_H_
 #define SUSUWATARI_SUSUGAMEPLAY_H_
 
+#include "Graphics_Susu.h"
 
+
+ typedef double mapObstacle[256][2*192] ;
+
+void bounceSusu(pSusu mySusu);
 
 #endif /* SUSUWATARI_SUSUGAMEPLAY_H_ */
